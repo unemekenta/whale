1f14504d4d86398042094970c6d715988670a04b
unemekenta <unemekenta@gmail.com>
2023-07-01 16:59:14 +0900

    Add: 日記に紐づく画像の更新処理

--- app/controllers/api/v1/diaries_controller.rb
+++ app/controllers/api/v1/diaries_controller.rb
@@ -7,18 +7,21 @@ module Api
 
       def index
         session_options_skip
-        diaries = Diary.where(user_id: @current_api_v1_user.id).limit(INDEX_LIMIT).offset(params[:offset]).order(date: :desc)
+        diaries = Diary.where(user_id: @current_api_v1_user.id)
+          .limit(INDEX_LIMIT)
+          .offset(params[:offset])
+          .order(date: :desc)
         return_data(STATUS_SUCCESS, '', diaries)
       end
 
       def show
         session_options_skip
-        diary = Diary.includes(:images)
+        diary = Diary.includes(diaries_image_relations: :image)
           .where("(id = ? AND user_id = ?) OR (id = ? AND public = ?)",
             params[:id], @current_api_v1_user.id,
             params[:id], true)
           .first
-          .as_json(include: :images)
+          .as_json(include: [:diaries_image_relations, :images])
         return_data(STATUS_SUCCESS, '', diary)
       end
 
@@ -29,10 +32,10 @@ module Api
           diary = Diary.new(title: params[:title], content: params[:content], public: params[:public], date: params[:date], user_id: user.id)
           diary.save
 
-          if params[:diaries_image_relation].present?
+          if params[:diaries_image_relations].present?
             diaries_image_relation_params = JSON.parse(params[:diaries_image_relation])
-            diaries_image_relation_params.each do |t|
-              diaries_image_relation = DiariesImageRelation.new(diary_id: diary.id, image_id: t['image_id'])
+            diaries_image_relation_params.each do |dir_id|
+              diaries_image_relation = DiariesImageRelation.new(diary_id: diary.id, image_id: dir_id)
               diaries_image_relation.save
             end
           end
@@ -41,14 +44,29 @@ module Api
       end
 
       def update
-        if @diary.user_id == @current_api_v1_user.id
-          if @diary.update(diary_params)
-            return_data(STATUS_SUCCESS, 'Updated the diary', @diary)
-          else
-            return_data(STATUS_SUCCESS, 'Not updated', @diary.errors)
+        ActiveRecord::Base.transaction do
+          if params[:diaries_image_relations].present?
+            diaries_image_relation_params = JSON.parse(params[:diaries_image_relations])
+            existing_diaries_image_relation = @diary.diaries_image_relations.pluck(:image_id)
+
+            # 削除するレコードを検出して削除する
+            @diary.diaries_image_relations.where(image_id: existing_diaries_image_relation - diaries_image_relation_params).destroy_all
+
+            diaries_image_relation_params.each do |dir_id|
+              next if existing_diaries_image_relation.include?(dir_id)
+              diaries_image_relation = DiariesImageRelation.new(diary_id: @diary.id, image_id: dir_id)
+              diaries_image_relation.save
+            end
           end
-        else
-          return_data(STATUS_FAILURE, 'You are not authorized to update this diary', '')
+          if @diary.user_id == @current_api_v1_user.id
+            if @diary.update(diary_params)
+              return_data(STATUS_SUCCESS, 'Updated the diary', @diary)
+            else
+              return_data(STATUS_SUCCESS, 'Not updated', @diary.errors)
+            end
+          else
+            return_data(STATUS_FAILURE, 'You are not authorized to update this diary', '')
+          end
         end
       end
 
@@ -70,7 +88,7 @@ module Api
       private
       def set_diary
         session_options_skip
-        @diary = Diary.find(params[:id])
+        @diary = Diary.includes(diaries_image_relations: :image).find(params[:id])
       end
 
       def diary_params
