bd81603e17ec4eb959eb957a06f5bf8d83359e85
unemekenta <unemekenta@gmail.com>
2023-08-31 00:42:20 +0900

    Fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,5 +52,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.hosts << "host.docker.internal"
+  config.hosts << "rails-api"
 end
