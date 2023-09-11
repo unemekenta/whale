4b5095dcb41db1a937d7f7722137f89228c93c6e
unemekenta <unemekenta@gmail.com>
2023-09-11 23:04:09 +0900

    Fix:

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,19 +1,26 @@
 CarrierWave.configure do |config|
 
+  CONOHA_TENANT_NAME = ENV['CONOHA_TENANT_NAME']
+  CONOHA_USERNAME = ENV['CONOHA_USERNAME']
+  CONOHA_API_PASSWORD = ENV['CONOHA_API_PASSWORD']
+  CONOHA_API_AUTH_URL = ENV['CONOHA_API_AUTH_URL']
+  CONOHA_CONTAINER_NAME = ENV['CONOHA_CONTAINER_NAME']
+  CONOHA_ASSET_HOST = ENV['CONOHA_ASSET_HOST']
+
   if ENV['RAILS_ENV'] == 'local' || ENV['RAILS_ENV'] == 'docker-local'
-    config.asset_host = ENV['API_HOST']
+    config.asset_host = ENV['CONOHA_ASSET_HOST']
     config.storage = :file
     config.cache_storage = :file
   else
     config.fog_credentials = {
       :provider => 'OpenStack',
-      :openstack_tenant => ENV['CONOHA_TENANT_NAME'],
-      :openstack_username => ENV['CONOHA_USERNAME'],
-      :openstack_api_key => ENV['CONOHA_API_PASSWORD'],
-      :openstack_auth_url => ENV['CONOHA_API_AUTH_URL'],
+      :openstack_tenant => CONOHA_TENANT_NAME,
+      :openstack_username => CONOHA_USERNAME,
+      :openstack_api_key => CONOHA_API_PASSWORD,
+      :openstack_auth_url => CONOHA_API_AUTH_URL,
     }
-    config.fog_directory = ENV['CONOHA_CONTAINER_NAME']
+    config.fog_directory = CONOHA_CONTAINER_NAME
     config.storage :fog
-    config.asset_host = ENV['CONOHA_ASSET_HOST'] + '/' + ENV['CONOHA_CONTAINER_NAME']
+    config.asset_host = CONOHA_ASSET_HOST + '/' + CONOHA_CONTAINER_NAME
   end
 end
