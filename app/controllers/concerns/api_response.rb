ee1c3c8b2120f3ae443f76e62e0e95428b27a16b
unemekenta <unemekenta@gmail.com>
2023-10-02 20:05:46 +0900

    Fix: 使用しなくなったメソッド削除

--- app/controllers/concerns/api_response.rb
+++ app/controllers/concerns/api_response.rb
@@ -1,17 +1,6 @@
 module ApiResponse
   extend ActiveSupport::Concern
 
-  STATUS_SUCCESS = 'SUCCESS'
-  STATUS_FAILURE = 'ERROR'
-
-  def return_data(message, data, status_code = :ok)
-    render json: { status: STATUS_SUCCESS, message: message, data: data }, code: status_code
-  end
-
-  def return_error(message, status_code = :unprocessable_entity)
-    render json: { status: STATUS_FAILURE, error: message }, code: status_code
-  end
-
   def session_options_skip
     request.session_options[:skip] = true
   end
