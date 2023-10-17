69d9735c1c271f2fe33c3f4d35a18220fa1b5ed3
unemekenta <unemekenta@gmail.com>
2023-10-17 23:21:54 +0900

    Add: ユーザー画像登録機能実装

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,13 +2,44 @@ module Api
   module V1
     class UsersController < ApplicationController
       before_action :authenticate_api_v1_user!
+      before_action :set_user, only: [:show, :edit, :update, :destroy]
       include ApiResponse
 
       def show
         session_options_skip
         @user = current_api_v1_user
-        render 'user', status: :ok
+        render 'show', status: :ok
       end
+    end
+
+    def update
+      session_options_skip
+      if user_params[:image].present?
+        # 画像を更新する場合
+        @user.image = user_params[:image]
+        if @user.save
+          render 'update', status: :ok
+        else
+          render 'update', status: :internal_server_error
+        end
+      else
+        # 画像を更新しない場合
+        if @user.update(user_params.except(:image))
+          render 'update', status: :ok
+        else
+          render 'update', status: :internal_server_error
+        end
+      end
+    end
+
+    private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :image)
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
+  mount_uploader :image, AvatarUploader
+
   include DeviseTokenAuth::Concerns::User
 
   has_many :tasks, dependent: :destroy
