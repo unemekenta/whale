7894d921cb977aeb3c8e7167d1705aeacbf7f252
unemekenta <unemekenta@gmail.com>
2023-11-18 02:09:12 +0900

    Fix

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -4,7 +4,7 @@ class Diary < ApplicationRecord
   has_many :images, through: :diaries_image_relations
   has_many :diary_comments, foreign_key: "diary_id", dependent: :destroy
 
-  validates :user_id, :title, :is_public, :date, presence: true
+  validates :user_id, :title, presence: true
   validates :title, length: { maximum: 50 }
   validates :content, length: { maximum: 10000 }
 end
