c04f5c05f449bb557d9f050300508a3c19048240
unemekenta <unemekenta@gmail.com>
2023-12-09 11:43:09 +0900

    Revert "Fix"
    This reverts commit 5c0eb6b16f95525fdfdbb76ee6a3319c34adc265.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins /^.*#{Regexp.escape(ENV['FRONT_HOST'])}.*$/
-    origins "*"
+    origins /^.*#{Regexp.escape(ENV['FRONT_HOST'])}.*$/
 
     resource '*',
       headers: :any,
