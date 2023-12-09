5c0eb6b16f95525fdfdbb76ee6a3319c34adc265
unemekenta <unemekenta@gmail.com>
2023-12-09 10:40:05 +0900

    Fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins /^.*#{Regexp.escape(ENV['FRONT_HOST'])}.*$/
+    # origins /^.*#{Regexp.escape(ENV['FRONT_HOST'])}.*$/
+    origins "*"
 
     resource '*',
       headers: :any,
